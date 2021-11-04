feb70b7c4a3f1f9f4482aaa058a4762ffd6559c4
Rishabh Joshi <joshir838@gmail.com>
2021-11-04 11:54:10 +0530

    v-- 2.0.1

--- js/extra.js
+++ js/extra.js
@@ -2,7 +2,14 @@
 
 // "use strict";
 
-const birthDay = [
+// const currentYear = new Date().getFullYear();
+
+const allBirthDays = [
+  {
+    name: "Isu",
+    date: 04,
+    month: "November",
+  },
   {
     name: "Kritika",
     date: 25,
@@ -14,6 +21,16 @@ const birthDay = [
     month: "November",
   },
   {
+    name: "Manjusha",
+    date: 08,
+    month: "December",
+  },
+  {
+    name: "Indira",
+    date: 12,
+    month: "December",
+  },
+  {
     name: "Rishabh",
     date: 16,
     month: "December",
@@ -21,21 +38,44 @@ const birthDay = [
 ];
 
 // /////////////////////////////////////////////////////////////
+
 // /////////////////////////////////////////////////////////////
+let i = 0;
 
-let currentYear = new Date().getFullYear();
+const countdown = () => {
+  let obj = allBirthDays[i];
 
-let name;
-let date;
-let month;
+  let { name, date, month } = obj;
 
-let gap;
+  let nextDate = new Date(`${month} ${date}, 2021 12:00:00`).getTime();
+  const now = new Date().getTime();
 
-const [obj1] = birthDay;
-({ name, date, month } = obj1);
+  const gap = nextDate - now;
 
-const nextBirthDay = new Date(`${month} ${date}, ${currentYear} 00:00:00`);
+  if (gap < 0) {
+    i++;
+    return;
+  }
+  /////////////////////////////////////////////////////////////////////////
+  //=======================================================================
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
-console.log(name, nextBirthDay);
+  // -----------------
+  const textDay = Math.floor(gap / day);
+  const textHour = Math.floor((gap % day) / hour);
+  const textMin = Math.floor((gap % hour) / minute);
+  const textSec = Math.floor((gap % minute) / second);
 
-// ---------------------------------------------------------------
+  // -----------------------------------------------------
+
+  document.querySelector(".heading").innerText = `${name}'s birthday countdown`;
+  document.querySelector(".timer__days").innerText = textDay;
+  document.querySelector(".timer__hrs").innerText = textHour;
+  document.querySelector(".timer__min").innerText = textMin;
+  document.querySelector(".timer__sec").innerText = textSec;
+};
+
+setInterval(countdown, 1000);
